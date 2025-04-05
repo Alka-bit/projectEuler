44484dc684ae1c2a0e72e277b1eeabb8cfa665b8
ALKA RAKESH YADAV <63008013+Alka-bit@users.noreply.github.com>
2025-04-05 18:26:11 +0530

    Create  Multiplesof3or5.py

---
+++ Multiplesof3or5.py
@@ -0,0 +1,8 @@
+#Find the sum of all the multiples of 3 or 5 below the provided parameter value number.
+
+def num(n):
+  list = []
+  for i in range(1, n):
+    if i%3==0 or i%5==0 & i not in list:
+      list.append(i)
+  return sum(list)
